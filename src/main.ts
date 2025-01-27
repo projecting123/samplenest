a8b546510fa29599b120ef969b574910e107e826
Ankur Rajbongshi <dekalasit@gmail.com>
2025-01-27 20:20:40 +0530

    5th

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
+  app.use(cors({credentials: true, origin: 'http://localhost:4200'}))
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
