375b38273634cf30cedab36563d8ff4f5640c2fb
Ankur Rajbongshi <dekalasit@gmail.com>
2025-01-27 21:11:43 +0530

    11th commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
