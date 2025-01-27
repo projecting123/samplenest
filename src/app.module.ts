1415d709e502539b5ac60fa0d3ddf5221629a884
Ankur Rajbongshi <dekalasit@gmail.com>
2025-01-27 20:33:48 +0530

    6th commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserDbModule } from './schema/user.db.module';
 
 @Module({
   imports: [
+    UserDbModule,
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGODB_URI!),
   ],
